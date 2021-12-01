90513b125cf2a5d255b4877a7ce7c8688a8179cc
Kaci <kaci.yan@outlook.com>
2021-12-01 04:08:18 +0100

    Set up the Application start and updated the test cases (may still change later) Moved the "Creation" bits into start

--- src/Application.java
+++ src/Application.java
@@ -1,35 +1,70 @@
+import Agents.StudentAgent;
+import Agents.TimetablerAgent;
+import Ontology.Elements.Student;
 import jade.core.Profile;
 import jade.core.ProfileImpl;
+import jade.core.Runtime;
 import jade.wrapper.AgentController;
 import jade.wrapper.ContainerController;
 import jade.wrapper.StaleProxyException;
-import jade.core.Runtime;
 
-import java.util.Hashtable;
+import java.util.ArrayList;
 
 public class Application
 {
+    private static int numberOfModules;
+    private static int tutorialGroupsPerModule;
+    private static int numberOfStudents;
+    private static int modulesPerStudent;
     
     public static void main(String[] args)
     {
         //setup jade environment
         Profile myProfile = new ProfileImpl();
         Runtime myRuntime = Runtime.instance();
-        ContainerController myContainer = myRuntime.createMainContainer(myProfile);
+        
+        //modules & students randomly generated here, would in reality be known & input via e.g. csv
+        var modules = ModuleGeneration.initialiseModules(numberOfModules, tutorialGroupsPerModule, numberOfStudents);
+        
+        var timetable = TimetableGeneration.generateRandomTimetable(modules);
+        
+        var students = new ArrayList<Student>();
+        for (int i = 0; i < numberOfStudents; i++) {
+            var student = StudentGeneration.generateRandomStudent();
+            StudentGeneration.randomlyAssignModulesToStudent(student, modules, modulesPerStudent);
+            students.add(student);
+        }
+        
+        initTestCase0();
+//            initTestCase1();
+//            initTestCase2();
+//            initTestCase3();
         
         try {
+            ContainerController myContainer = myRuntime.createMainContainer(myProfile);
             
             //start agent controller (also an agent (rma)
             AgentController rma = myContainer.createNewAgent("rma", "jade.tools.rma.rma", null);
             rma.start();
             
-            AgentController auctioneerAgent = myContainer.createNewAgent("auctioneer", AuctioneerAgent.class.getCanonicalName(), new Hashtable[]{catalogue});
-            auctioneerAgent.start();
+            AgentController timetablerAgent = myContainer.createNewAgent("timetabler", TimetablerAgent.class.getCanonicalName(), null);
+            timetablerAgent.start();
             
-            var biddersToGenerate = 3;
-            generateBiddersWithRandomisedShoppingLists(myContainer, biddersToGenerate);
-
-            
+            students.forEach(student -> {
+                AgentController studentAgent = null;
+                try {
+                    studentAgent = myContainer.createNewAgent("student_" + student.getMatriculationNumber(), StudentAgent.class.getCanonicalName(), null);
+                }
+                catch (StaleProxyException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    studentAgent.start();
+                }
+                catch (StaleProxyException e) {
+                    e.printStackTrace();
+                }
+            });
         }
         catch (Exception e) {
             System.out.println("Exception starting agent: " + e.toString());
@@ -41,57 +76,34 @@ public class Application
     
     private static void initTestCase0() {
         
-        var numberOfModules = 1;
-        var tutorialGroupsPerModule = 2;
-        Creation.initialiseTimetable(numberOfModules, tutorialGroupsPerModule);
+        numberOfModules = 1;
+        tutorialGroupsPerModule = 2;
+        numberOfStudents = 100;
+        modulesPerStudent = 1;
     }
     
     private static void initTestCase1() {
         
-        var numberOfModules = 2;
-        var tutorialGroupsPerModule = 2;
-        Creation.initialiseTimetable(numberOfModules, tutorialGroupsPerModule);
+        numberOfModules = 2;
+        tutorialGroupsPerModule = 2;
+        numberOfStudents = 200;
+        modulesPerStudent = 2;
+    }
+    
+    private static void initTestCase2() {
         
+        var numberOfModules = 3;
+        var tutorialGroupsPerModule = 3;
+        numberOfStudents = 200;
+        modulesPerStudent = 3;
     }
     
     private static void initTestCase3() {
         
         var numberOfModules = 3;
-        var tutorialGroupsPerModule = 3;
-        Creation.initialiseTimetable(numberOfModules, tutorialGroupsPerModule);
-        
-    }
-    
-    private static void initTestCase4() {
-        
-        var numberOfModules = 3;
         //random amount of tutorial groups per module
         var tutorialGroupsPerModule = -1;
-        Creation.initialiseTimetable(numberOfModules, tutorialGroupsPerModule);
-        
-    }
-    
-    private static void generateBiddersWithRandomisedShoppingLists(ContainerController myContainer, int biddersToGenerate) throws StaleProxyException
-    {
-        for (int i = 0; i < biddersToGenerate; i++) {
-            
-            var shoppingList = CsvParsers.GenerateShoppingList();
-            
-            AgentController bidderAgent = myContainer.createNewAgent("bidder" + i, BidderAgent.class.getCanonicalName(), new Hashtable[]{shoppingList});
-            bidderAgent.start();
-        }
-    }
-    
-    private static void generateBiddersWithCsvShoppingLists(ContainerController myContainer) throws StaleProxyException
-    {
-        for (int i = 0; i < 5; i++) {
-            
-            var shoppingListCsvPath = "pcBuilderShoppingList" + i + ".csv";
-            
-            var shoppingList = CsvParsers.ParseShoppingListCsv(shoppingListCsvPath);
-            
-            AgentController bidderAgent = myContainer.createNewAgent("bidder" + i, BidderAgent.class.getCanonicalName(), new Hashtable[]{shoppingList});
-            bidderAgent.start();
-        }
+        numberOfStudents = 200;
+        modulesPerStudent = 3;
     }
 }
--- src/TimetableGeneration.java
+++ src/TimetableGeneration.java
@@ -1,30 +1,14 @@
 import Ontology.Elements.Module;
-import Ontology.Elements.Student;
 import Ontology.Elements.TutorialTimetable;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class Creation
+public class TimetableGeneration
 {
-    
-    public static void initialise(int numberOfStudents, int numberOfModules, int tutorialGroupsPerModule, int modulesPerStudent) {
-        var modules = ModuleGeneration.initialiseModules(numberOfModules, tutorialGroupsPerModule, numberOfStudents);
-        var timetable = generateRandomTimetable(modules);
-        
-        var students = new ArrayList<Student>();
-        for (int i = 0; i < numberOfStudents; i++) {
-            var student = StudentGeneration.generateRandomStudent();
-            StudentGeneration.randomlyAssignModulesToStudent(student, modules, modulesPerStudent);
-            students.add(student);
-        }
-        
-    }
-    
-    private static TutorialTimetable generateRandomTimetable(List<Module> modules) {
+    static TutorialTimetable generateRandomTimetable(List<Module> modules) {
         var emptyTimeslotIds = IntStream.range(1, 45).boxed().collect(Collectors.toList());
         
         var timetable = new TutorialTimetable();
@@ -35,7 +19,7 @@ public class Creation
         modules.forEach((module) -> {
             module.getTutorials().forEach((tutorial -> {
                 int i = r.nextInt(emptyTimeslotIds.size()) + 1;
-                var timeslotId = (int)emptyTimeslotIds.get(i);
+                var timeslotId = (int) emptyTimeslotIds.get(i);
                 
                 tutorial.setTimeSlotId(timeslotId);
                 
