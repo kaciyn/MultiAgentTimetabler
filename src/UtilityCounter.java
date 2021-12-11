5eb1d53974562c0de7fca1427e495fe9a1e14213
Kaci <kaci.yan@outlook.com>
2021-12-11 04:02:12 +0100

    drafted utility counting and recording functions, will probably move it out to be an agent's job later?

---
+++ src/UtilityCounter.java
@@ -0,0 +1,37 @@
+import Ontology.Elements.StudentTimetablePreferences;
+
+import java.util.ArrayList;
+
+public class UtilityCounter
+{
+    private Integer totalSystemUtility;
+    private Integer averageSystemUtility;
+    
+    public void averageUtilityTimes(long startTime,long currentTime) {
+        //todo somewhere trigger at say 0,0.25,.5,.75, .9, .95, .99, and 1 - maybe an agent
+    }
+        
+        
+        public void recordAverageUtilityTimesReached(long startTime,long currentTime) {
+        
+        System.out.println("Average system utility of: " + averageSystemUtility + " reached at: "+ (currentTime - startTime));
+        
+    }
+    
+    public Integer getAverageSystemUtility(ArrayList<StudentTimetablePreferences> studentsTimetablePreferences) {
+        averageSystemUtility = getTotalSystemUtility(studentsTimetablePreferences) / studentsTimetablePreferences.size();
+        return averageSystemUtility;
+        
+    }
+    
+    public Integer getTotalSystemUtility(ArrayList<StudentTimetablePreferences> studentsTimetablePreferences) {
+        totalSystemUtility = 0;
+        
+        for (StudentTimetablePreferences studentTimetablePreferences : studentsTimetablePreferences) {
+            totalSystemUtility += studentTimetablePreferences.getTotalUtility();
+            
+        }
+        return totalSystemUtility;
+    }
+    
+}
